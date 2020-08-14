8c97855010dda28ad199241feadb7fe5873bb387
lewyk <lewyk@hotmail.com>
2020-08-14 22:01:47 +0300

    копирует ДЗ7

---
+++ js/sendform.js
@@ -0,0 +1,25 @@
+(() => {
+  document.querySelector(".js-speaker-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`)
+    );
+
+    e.currentTarget.reset();
+  });
+})();
+
+(() => {
+  document
+    .querySelector(".js-speaker-form2")
+    .addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      new FormData(e.currentTarget).forEach((value, name) =>
+        console.log(`${name}: ${value}`)
+      );
+
+      e.currentTarget.reset();
+    });
+})();
